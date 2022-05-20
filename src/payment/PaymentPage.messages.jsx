42ecf29e3cb80fc7c62428f0a60f8e84efb8a19d
khalilbsd <78567534+khalilbsd@users.noreply.github.com>
2022-05-20 14:52:41 +0100

    first light

---
+++ src/payment/PaymentPage.messages.jsx
@@ -0,0 +1,31 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  'payment.page.heading': {
+    id: 'payment.page.heading',
+    defaultMessage: 'Payment',
+    description: 'The page heading for payment.',
+  },
+  'payment.loading.payment': {
+    id: 'payment.loading.payment',
+    defaultMessage: 'Loading basket...',
+    description: 'Message when payment page is being loaded',
+  },
+  'payment.loading.error': {
+    id: 'payment.loading.error',
+    defaultMessage: 'Error: {error}',
+    description: 'Message when payment page failed to load',
+  },
+  'payment.apple.pay.merchant.validation.failure': {
+    id: 'payment.apple.pay.merchant.validation.failure',
+    defaultMessage: 'Apple Pay is not available at this time. Please try another payment method.',
+    description: 'The message displayed to the user when an Apple Pay session fails to begin',
+  },
+  'payment.apple.pay.authorization.failure': {
+    id: 'payment.apple.pay.authorization.failure',
+    defaultMessage: 'An error occurred while processing your payment. You have not been charged. Please try again, or select another payment method.',
+    description: 'The message displayed to the user when an Apple Pay payment fails',
+  },
+});
+
+export default messages;
---
+++ src/payment/payment-methods/paypal/PayPalButton.messages.js
@@ -0,0 +1,16 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  'payment.type.paypal': {
+    id: 'payment.type.paypal',
+    defaultMessage: 'PayPal',
+    description: 'The alt text for image button for paypal payment type',
+  },
+  'payment.page.methond.type.name': {
+    id:'payment.page.methond.type.name',
+    defaultMessage: 'Pay now',
+    description: 'The alt text for the payment button',
+  }
+});
+
+export default messages;
